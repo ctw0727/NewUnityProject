7ca6006382c3afaef8631c5d46b2f2cf3bf10e53
천태웅 <ctw0727@naver.com>
2020-07-04 16:37:56 +0900

    Practice1
    did

--- New_Unity_Project/Assets/Scripts/ctw/ctw_Player_behavior.cs
+++ New_Unity_Project/Assets/Scripts/ctw/ctw_Player_behavior.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ctw_Player_behavior : MonoBehaviour
 {
+	Transform PlayerTransform;
+	Rigidbody2D PlayerRigid2D;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerTransform = GetComponent<Transform>();
+		PlayerRigid2D = GetComponent<Rigidbody2D>();
+		
     }
+
+	void Move(){
+		
+		float XMoveCount = 0;
+		float YMoveCount = 0;
+		
+		if (Input.GetKey(KeyCode.A))
+			XMoveCount--;
+		if (Input.GetKey(KeyCode.D))
+			XMoveCount++;
+		if (Input.GetKey(KeyCode.W))
+			YMoveCount++;
+		if (Input.GetKey(KeyCode.S))
+			YMoveCount--;
+		
+		PlayerRigid2D.velocity = new Vector2(20*XMoveCount,20*YMoveCount);
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
 }
