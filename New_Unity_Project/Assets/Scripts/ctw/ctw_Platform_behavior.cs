1ec22f99cce98bf5b255c0943b0a2ec04ad1d7ea
천태웅 <ctw0727@naver.com>
2020-07-05 19:51:47 +0900

    Barrage Platformer Base
    Basics

---
+++ New_Unity_Project/Assets/Scripts/ctw/ctw_Platform_behavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ctw_Platform_behavior : MonoBehaviour
+{
+	
+	public bool Trigger = false;
+	
+	int DownCool = 0;
+	
+	Transform PlatformTransform;
+	BoxCollider2D PlatformCollider;
+	
+	Transform PlayerTransform;
+	
+    // Start is called before the first frame update
+    void Start(){
+		
+        PlatformTransform = GetComponent<Transform>();
+		PlatformCollider = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+		PlatformCollider.offset = new Vector2(0,-1);
+		PlatformCollider.size = new Vector2(1,2);
+		
+		PlayerTransform = GameObject.Find("ctw_Player").GetComponent<Transform>();
+    }
+	
+	void Control(){
+		
+		ctw_Player_behavior CallScript = GameObject.Find("ctw_Player").GetComponent<ctw_Player_behavior>();
+		
+		if ((PlayerTransform.position.y >= PlatformTransform.position.y + 0.49f)&&(DownCool == 0)){
+			PlatformCollider.isTrigger = false;
+			
+			if ((CallScript.DOWN == true)&&(PlayerTransform.position.y - PlatformTransform.position.y <= 2f)){
+				PlatformCollider.isTrigger = true;
+				DownCool = 1;
+				Invoke("Cooler",0.5f);
+			}
+		}
+		if ((PlayerTransform.position.y < PlatformTransform.position.y -0.49f)&&(DownCool == 0)){
+			PlatformCollider.isTrigger = true;
+		}
+	}
+	
+	void Cooler(){
+		DownCool = 0;
+	}
+
+    // Update is called once per frame
+    void Update(){
+		
+		Control();
+		
+		Trigger = PlatformCollider.isTrigger;
+    }
+}
--- New_Unity_Project/Assets/Scripts/ctw/ctw_Player_behavior.cs
+++ New_Unity_Project/Assets/Scripts/ctw/ctw_Player_behavior.cs
@@ -6,35 +6,169 @@ public class ctw_Player_behavior : MonoBehaviour
 {
 	Transform PlayerTransform;
 	Rigidbody2D PlayerRigid2D;
+	Collider2D PlayerCollider;
+	Camera MainCamera;
 	
-    // Start is called before the first frame update
-    void Start()
-    {
+	public bool DOWN = false;
+	
+	float HP = 1;
+	int OnAir = 0;
+	int OnAttack = 0;
+	
+    void Start(){
+		
         PlayerTransform = GetComponent<Transform>();
 		PlayerRigid2D = GetComponent<Rigidbody2D>();
-		
+		PlayerCollider = GetComponent<Collider2D>();
+		MainCamera = GameObject.Find("ctw_Main Camera").GetComponent<Camera>();
     }
-
-	void Move(){
+	
+	// Math
+	
+	float Math_2D_Force(float x, float y){ // Success
+		
+		return Mathf.Sqrt(Mathf.Pow(x,2)+Mathf.Pow(y,2));
+	}
+	
+	// Timer
+	
+	void Timer(){
+		
+	}
+	
+	// Get
+	
+	Vector2 GetForceDirection(){ // Success
+		
+		Vector3 PlayerPos = PlayerTransform.position;
+		Vector3 MouseStaticPos = new Vector3(MainCamera.ScreenToWorldPoint(Input.mousePosition).x,MainCamera.ScreenToWorldPoint(Input.mousePosition).y,0);
+		Vector3 MousePrivatePos = MouseStaticPos - PlayerPos;
+		
+		float RangeKey = Math_2D_Force(MousePrivatePos.x,MousePrivatePos.y);
+		
+		MousePrivatePos = MousePrivatePos/RangeKey;
+		
+		Vector2 ForceDirection = new Vector2(MousePrivatePos.x,MousePrivatePos.y);
+		
+		return ForceDirection;
+	}
+	
+	// Checks
+	
+	void Colliding(Collider2D other, int type){
+		
+	}
+	
+	// Actions
+	
+	void StrikeEnemy(){
+		
+	}
+	
+	void StrikeGround(){
+		
+	}
+	
+	void StrikePlatform(){
+		
+	}
+	
+	// Input
+	
+	void InputMove(){ // Success
 		
 		float XMoveCount = 0;
-		float YMoveCount = 0;
 		
-		if (Input.GetKey(KeyCode.A))
-			XMoveCount--;
-		if (Input.GetKey(KeyCode.D))
-			XMoveCount++;
-		if (Input.GetKey(KeyCode.W))
-			YMoveCount++;
-		if (Input.GetKey(KeyCode.S))
-			YMoveCount--;
+		if (OnAttack == 0){
+				
+			if (Input.GetKey(KeyCode.A))
+				XMoveCount--;
+			if (Input.GetKey(KeyCode.D))
+				XMoveCount++;
+			
+			PlayerRigid2D.velocity = new Vector2(15*XMoveCount,PlayerRigid2D.velocity.y);
+			
+			if (Input.GetKeyDown(KeyCode.S)){
+				DOWN = true;
+			}
+			else{
+				DOWN = false;
+			}
+			
+			if ((Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.Space))&&(OnAir == 0)){
+				PlayerRigid2D.velocity = PlayerRigid2D.velocity + new Vector2(0,35);
+				OnAir = 1;
+			}
+		}
+	}
+	
+	void InputAttack(){
 		
-		PlayerRigid2D.velocity = new Vector2(20*XMoveCount,20*YMoveCount);
+		if ((Input.GetMouseButton(0))&&(OnAir == 1)&&(OnAttack != 2)){
+			if ((PlayerRigid2D.angularVelocity <= 2000)&&(MainCamera.ScreenToWorldPoint(Input.mousePosition).x - PlayerTransform.position.x <= 0)){
+				PlayerRigid2D.angularVelocity = PlayerRigid2D.angularVelocity + 30;
+			}
+			if ((PlayerRigid2D.angularVelocity >= -2000)&&(MainCamera.ScreenToWorldPoint(Input.mousePosition).x - PlayerTransform.position.x > 0)){
+				PlayerRigid2D.angularVelocity = PlayerRigid2D.angularVelocity - 30;
+			}
+			OnAttack = 1;
+		}
+		else if ((Input.GetMouseButtonUp(0))&&(OnAttack == 1)){
+			PlayerRigid2D.velocity = GetForceDirection()*Mathf.Abs(PlayerRigid2D.angularVelocity)/40;
+			OnAttack = 2;
+		}
+		
+		if (OnAttack == 1){
+			PlayerRigid2D.angularDrag = 0.1f;
+			PlayerRigid2D.drag = 15f;
+		}
+		else{
+			PlayerRigid2D.angularDrag = 0.2f;
+			PlayerRigid2D.drag = 0f;
+		}
+	}
+	
+	// Running
+	
+	void OnTriggerStay2D(Collider2D other){ // Success
+		
+		switch (other.tag){
+			case "Platform":
+				ctw_Platform_behavior Script = other.GetComponent<ctw_Platform_behavior>();
+				
+				if ((Script.Trigger == false)&&(PlayerRigid2D.velocity.y <= 0)){
+					OnAir = 0;
+					OnAttack = 0;
+				}
+			break;
+			
+			case "Enemy":
+				OnAttack = 0;
+			break;
+			
+			case "Ground":
+				OnAir = 0;
+				OnAttack = 0;
+			break;
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D other){ // Success
+		
+		switch (other.tag){
+			case "Platform":
+				OnAir = 1;
+			break;
+			
+			case "Ground":
+				OnAir = 1;
+			break;
+		}
 	}
 
-    // Update is called once per frame
     void Update()
     {
-        Move();
+		InputAttack();
+        InputMove();
     }
 }
