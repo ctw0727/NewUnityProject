303c9f83da7829cfbac978c5882376b6e3a70f72
SSH-20 <seokhyun.song01@gmail.com>
2020-07-18 19:51:13 +0900

    게임 기반
    이동, 점프 구현/ 간단한 물리 구현
    
    현재 맵 구상중

---
+++ New_Unity_Project/Assets/Scripts/ssh/ssh_move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ssh_move: MonoBehaviour
+{
+	
+	public float MaxSpeed;
+	public float JumpPower;
+	Rigidbody2D rigid;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {	
+		//점프
+		if (Input.GetButtonDown("Jump"))
+			rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+		
+		//움직임
+		float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+		
+		if(rigid.velocity.x > MaxSpeed)						//오른쪽 속도 제어
+			rigid.velocity = new Vector2(MaxSpeed, rigid.velocity.y);
+			
+		else if(rigid.velocity.x < MaxSpeed * (-1))			//왼쪽 속도 제어
+			rigid.velocity = new Vector2(MaxSpeed * (-1), rigid.velocity.y);
+	}
+}
--- New_Unity_Project/Assets/Scripts/ssh/ssh_player.cs
+++ New_Unity_Project/Assets/Scripts/ssh/ssh_player.cs
@@ -4,36 +4,16 @@ using UnityEngine;
 
 public class ssh_player : MonoBehaviour
 {
-	Rigidbody2D PlayerRigid2D;
-	Transform PlayerTransform;
 	
-    // Start is called before the first frame update
+	
     void Start()
     {
-        PlayerTransform = GetComponent<Transform>();
-		PlayerRigid2D = GetComponent<Rigidbody2D>();
+		
     }
-
-	void Move()
-	{
-		float XMovecount = 0;
-		float YMovecount = 0;
-		
-		if (Input.GetKey(KeyCode.A))
-			XMovecount--;
-		if (Input.GetKey(KeyCode.D))
-			XMovecount++;
-		if (Input.GetKey(KeyCode.W))
-			YMovecount++;
-		if (Input.GetKey(KeyCode.S))
-			YMovecount--;
-		
-		PlayerRigid2D.velocity = new Vector2(20*XMovecount, 20*YMovecount);
-	}
 	
-    // Update is called once per frame
+ 
     void Update()
     {
-        Move();
+		
     }
 }
