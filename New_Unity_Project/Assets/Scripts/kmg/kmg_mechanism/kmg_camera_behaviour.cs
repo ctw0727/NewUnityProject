45135399587e1177ef6822f90d7df916639121d6
김민기 <alsrl6710@gmail.com>
2020-07-30 15:14:05 +0900

    200730-2 (core update 3)
    - 깜박이는 블록을 전후로 스테이지를 나누어 놓았음
    (특정 구간을 지나면 체크포인트로 돌아가게 하는 방식)
    - 스크립트 폴더 재정비

---
+++ New_Unity_Project/Assets/Scripts/kmg/kmg_mechanism/kmg_camera_behaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kmg_camera_behaviour : MonoBehaviour
+{
+	float greenValue, size = 8f;
+	float playerX, playerY;
+	Camera cam;
+	
+	public GameObject player;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		cam = gameObject.GetComponent<Camera>();
+		
+		cam.orthographicSize = size;
+	}
+	
+	void Initialize()
+	{
+		playerX = player.transform.position[0];
+		playerY = player.transform.position[1];
+		
+		greenValue = (playerY + 15f) / 150f;
+		if(greenValue >= 0.7f)
+			greenValue = 0.7f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Initialize();
+		transform.position = new Vector3(playerX, playerY, 0f);
+		cam.backgroundColor = new Color(0f, greenValue, 0f, 1f);
+		// 메인카메라의 배경색으로 플레이어의 y좌표를 최대 90까지 알 수 있음
+	}
+}
